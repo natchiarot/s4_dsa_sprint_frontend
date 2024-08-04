0612f4853dbdc9a17e11c039383cf81e738edb23
Nat <142550205+natchiarot@users.noreply.github.com>
2024-08-04 17:28:12 -0230

    Add files via upload

---
+++ dsa_sprint_frontend/src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return <Header />;
+}
+
+export default App;
